b1c9dfa974b368efd8274a9ca26af4f6e1124784
Artur Pedrosa <maverickanp@gmail.com>
2019-08-27 14:33:19 -0300

    initial conmmit

---
+++ notes.js
@@ -0,0 +1,18 @@
+// criacao de usuario
+
+/**
+routes.get('/users', async (req, res) => {
+  const user = await User.create({
+    name: 'Artur Pedrosa',
+    email: 'apedrosa.dev@gmail.com',
+    password_hash: '12345',
+  });
+  return res.json(user);
+});
+* */
+
+/**
+routes.get('/', (req, res) => {
+  return res.json({ message: 'teste' });
+});
+ */
