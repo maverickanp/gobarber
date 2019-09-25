b039fc3ab25a3006293b2a9213ef10ead31d8885
Artur Pedrosa <maverickanp@gmail.com>
2019-09-25 17:54:15 -0300

    Adjusts auth.js , token not provided withou return, added .sequelizerc config

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -7,7 +7,7 @@ export default async (req, res, next) => {
   const authHeader = req.headers.authorization;
 
   if (!authHeader) {
-    res.status(401).json({ error: 'token not provided.' });
+    return res.status(401).json({ error: 'token not provided.' });
   }
 
   const [, token] = authHeader.split(' ');
